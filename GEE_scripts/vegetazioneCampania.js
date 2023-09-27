f4783c777fe1e221383268eeb60f476bb63a3e29
VanniMaceria <andreabucci123@gmail.com>
2023-09-27 15:41:33 +0200

    Migliorati commenti allo script VegetazioneCampania

--- GEE_scripts/vegetazioneCampania.js
+++ GEE_scripts/vegetazioneCampania.js
@@ -1,5 +1,5 @@
 /*
-da Google Earth Engine bisogna importare:
+Su Google Earth Engine bisogna importare:
 - lo shape file della Campania;
 - la seguente ImageCollection ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA")
 */
@@ -8,21 +8,18 @@ var dataInizio = "2022-01-01";  //YYYY-MM-DD
 var dataFine = "2022-12-31";
 
 var L8 = ee.ImageCollection(landsatTOA)
-  .filterBounds(shapefileCampania)
-  .filterDate(dataInizio, dataFine)
-  .filter(ee.Filter.lt('CLOUD_COVER', 1)) // Filtra per copertura nuvolosa inferiore al 1%
-  .mean()
-  .clip(shapefileCampania);
+  .filterBounds(shapefileCampania)  //filtro l'ImageCollection per confini (Campania)
+  .filterDate(dataInizio, dataFine) //per data
+  .filter(ee.Filter.lt('CLOUD_COVER', 1)) //per copertura nuvolosa inferiore al 1%
+  .mean() //mean() calcola la media dei pixel di una collezione di immagini
+  .clip(shapefileCampania); //clip() ritaglia l'immagine in base ai limiti dell'area d'interesse
   
-  //mean() calcola la media dei pixel di una collezione di immagini
-  //clip() ritaglia l'immagine in base ai limiti dell'area d'interesse
   
-var nir = L8.select('B5');  //rappresenta l'immagine nella banda del vicino infrarosso
-var red = L8.select('B4');  //rappresenta l'immagine nella banda del rosso
+var nir = L8.select('B5');  //seleziona dall'ImageCollection ottenuta le banda del vicino infrarosso
+var red = L8.select('B4');  //seleziona dall'ImageCollection ottenuta le bande del rosso
 
-//calcolo del NDVI (Indice di Differenza Normalizzata di Vegetazione)
 /*
-Questa formula calcola l'NDVI utilizzando le bande NIR e Red.
+Questa formula calcola l'NDVI (Indice di Differenza Normalizzata di Vegetazione) utilizzando le bande NIR e Red.
 Il risultato è un'immagine dove i valori più alti indicano una maggiore densità di vegetazione
 */
 var ndvi = nir.subtract(red).divide(nir.add(red)).rename('NDVI');
@@ -34,5 +31,6 @@ var ndviParams = {
 }
 
 Map.centerObject(shapefileCampania, 8); //centro la mappa sul layer della Campania
-Map.addLayer(ndvi, ndviParams);  //carico sulla mappa il layer che si trova negli assets del progetto
+Map.addLayer(ndvi, ndviParams, "VegetazioneCampania");
+
 //devo fare una gif del timelaps
